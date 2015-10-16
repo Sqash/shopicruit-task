6142258bd21521ece2bc44c4da81b5be197e913d
Sam Whiteley <sam.whiteley@hotmail.com>
2015-10-16 15:21:39 -0400

    README updated and script added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# shopicruit-task
-A little code piece working with Shopify's JSON data
+# Shopicruit Task
+
+A little code piece working with Shopify's JSON data.
+
+Find out how much a ton of wallets and lamps cost!
+
+Answer(s):
+
+$1113.64 including 13% tax, $985.52 without tax.
---
+++ shopicruit.py
@@ -0,0 +1,39 @@
+import sys
+import urllib.request
+import json
+
+if(len(sys.argv) != 2):
+    print("Improper usage. Store 'products.json' url argument required")
+    exit(1)
+else:
+    print("Url given: {}".format(sys.argv[1]))
+
+try:
+    res = urllib.request.urlopen(sys.argv[1])
+    data = json.loads(res.readall().decode('utf-8'))
+    print("Received data... Parsing for wallets and lamps")
+except BaseException as e:
+    print("Error occured while getting data: {}".format(str(e)))
+    exit(1)
+
+products = data["products"]
+numProducts = 0
+numBought = 0
+taxableCost = 0
+nonTaxCost = 0
+
+for item in products:
+    if (item["product_type"] == "Wallet") or (item["product_type"] == "Lamp"):
+        numProducts += 1
+        for v in item["variants"]:
+            numBought += 1
+            if v["taxable"]:
+                taxableCost += float(v["price"])
+            else:
+                nonTaxCost += float(v["price"])
+                print("non-taxable item!")
+
+taxRate = 1.13
+cost = taxableCost*taxRate + nonTaxCost
+
+print("Buying {} variants of {} core products, you'd spend ${:.2f} including 13% tax".format(numBought, numProducts, cost, int((taxRate - 1)*100)))
